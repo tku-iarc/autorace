223c0d60ea50502ec258887b34436c183414c38c
cyc <ycpss91255@gmail.com>
2023-05-03 21:18:11 +0800

    init: yolo ros env

---
+++ docker_pytorch-ros/config/shell/bash_setup.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# ${1}: USER
+# ${2}: GROUP
+
+{
+    # add Hello Docker to bash config
+    echo "echo 'Hello Docker!'"
+
+    # add common bash aliase to bash config
+    echo -e "alias eb='vim ~/.bashrc'\n\
+    alias sb='source ~/.bashrc && echo \"You source user config!\"'\n\
+    alias wb='source ~/work/devel/setup.bash && echo \"You source workspace config!\"'\n"
+
+    # add color and git branch to bash config
+    echo -e "force_color_prompt=yes\n\
+    color_prompt=yes\n\
+    parse_git_branch() {\n\
+        git branch 2>/dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'\n\
+    }\n\
+    if [ \"\$color_prompt\" = yes ]; then\n\
+        PS1='\${debian_chroot:+(\$debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[01;31m\]\$(parse_git_branch)\[\033[00m\]\$ '\n\
+    else\n\
+        PS1='\${debian_chroot:+(\$debian_chroot)}\u@\h:\w\$(parse_git_branch)\$ '\n\
+    fi\n\
+    unset color_prompt force_color_prompt"
+} >> /home/"${1}"/.bashrc
+
+chown "${1}":"${2}" /home/"${1}"/.bashrc
---
+++ docker_pytorch-ros/run.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Get dependent parameters
+source "$(dirname "$(readlink -f "${0}")")/get_param.sh"
+
+docker run --rm \
+    --privileged \
+    --network=host \
+    --ipc=host \
+    ${GPU_FLAG} \
+    -v /home/"${user}"/.Xauthority:/home/"${user}"/.Xauthority \
+    -e XAUTHORITY=/home/"${user}"/.Xauthority \
+    -e DISPLAY="${DISPLAY}" \
+    -e QT_X11_NO_MITSHM=1 \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    -v /etc/timezone:/etc/timezone:ro \
+    -v /etc/localtime:/etc/localtime:ro \
+    -v /dev:/dev \
+    -v "${WS_PATH}":/home/"${user}"/work \
+    -it --name "${CONTAINER}" "${user}"/"${IMAGE}"
