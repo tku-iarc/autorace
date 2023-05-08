25c47b55bbafc55f72477ef3bc81f26f97133d12
cyc <ycpss91255@gmail.com>
2023-05-08 16:03:18 +0800

    add: ros and opencr config

--- docker_pytorch-ros/config/shell/bash_setup.sh
+++ docker_pytorch-ros/config/shell/bash_setup.sh
@@ -6,7 +6,11 @@
 {
     # add Hello Docker to bash config
     echo "echo 'Hello Docker!'"
+    echo "echo 'ROS_MASTER_URL=$ROS_MASTER_URL'"
+    echo "echo 'ROS_HOSTNAME=$ROS_HOSTNAME'"
+    echo "echo 'ROS_IP=$ROS_IP'"
 
+    echo "source /opt/ros/noetic/setup.bash"
     # add common bash aliase to bash config
     echo -e "alias eb='vim ~/.bashrc'\n\
     alias sb='source ~/.bashrc && echo \"You source user config!\"'\n\
