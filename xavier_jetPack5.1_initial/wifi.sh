4be9f8d4f9ea01f8e81da67bf17332299eab5aa9
cyc <ycpss91255@gmail.com>
2023-05-08 16:02:07 +0800

    del: change to initial folder

--- xavier_jetPack5.1_initial/wifi.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-wifi_file="$(dirname "$(readlink -f "${0}")")"/iwlwifi-ty-a0-gf-a0-59.ucode
-sudo apt update && \
-sudo apt upgrade && \
-sudo cp "${wifi_file}" /lib/firmware/ && \
-sudo mv /lib/firmware/iwlwifi-ty-a0-gf-a0.pnvm /lib/firmware/iwlwifi-ty-a0-gf-a0.pnvm.bak && \
-sudo reboot -h
